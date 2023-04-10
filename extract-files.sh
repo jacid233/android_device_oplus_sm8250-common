cfeb8bab34152be9936f839e0288e97859498f3e
Mashopy <eliasgheeraert@gmail.com>
2023-04-10 19:48:00 +0200

    sm8250-common: Patch camera.qcom.so instead of com.qti.chi.override.so
    It seems that realme rely over camera.qcom.so for auxiliary camera availability.
    
    Test: Open CameraIDs app and all camera IDs are now showed.
          Open a custom Google Camera app and manually switch to each cameras.
    
    Change-Id: I6aa269a41bdc81405137bf6d1d81705470982488

--- extract-files.sh
+++ extract-files.sh
@@ -72,9 +72,8 @@ function blob_fixup() {
         odm/lib64/libui.so)
             patchelf --replace-needed "android.hardware.graphics.common-V1-ndk_platform.so" "android.hardware.graphics.common-V1-ndk.so" "${2}"
             ;;
-		vendor/lib64/hw/com.qti.chi.override.so)
-            grep -q libcamera_metadata_shim.so "${2}" || "${PATCHELF}" --add-needed libcamera_metadata_shim.so "${2}"
-            sed -i "s/com.oem.autotest/\x00om.oem.autotest/" "${2}"
+        vendor/lib64/hw/camera.qcom.so)
+            grep -q libcamera_metadata_shim.so "${2}" || "${PATCHELF}" --add-needed "libcamera_metadata_shim.so" "${2}"
             ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
             "${SIGSCAN}" -p "AB 0B 00 94" -P "1F 20 03 D5" -f "${2}"
