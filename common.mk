79a5e65e5018d0dc68332c8a1dd788bd4ca98501
jacid233 <jacid11@outlook.com>
2023-06-06 16:19:19 +0800

    sm8250-common: kang loggy.sh from lineage gerrit

--- common.mk
+++ common.mk
@@ -455,3 +455,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
+
+# loggy
+PRODUCT_PACKAGES += \
+    loggy.sh
