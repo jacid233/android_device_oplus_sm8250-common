e50f96f261c9c07c800b516993ba6e5a40ba132f
jacid233 <jacid11@outlook.com>
2023-07-28 20:48:11 +0800

    sm8250-common: overlay: Import captive_portal url from stock

--- common.mk
+++ common.mk
@@ -164,6 +164,9 @@ PRODUCT_PACKAGES += \
     OPlusTetheringResCommon \
     OPlusWifiResCommon
 
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-aosp
+
 # Partition
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
